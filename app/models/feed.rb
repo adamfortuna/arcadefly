0edebb04204ea4bbade6f1601a9628036e9acaa0
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-07-26 23:16:07 +0000

    Removed unused.

--- app/models/feed.rb
+++
@@ -1,4 +0,0 @@
-class Feed < ActiveRecord::Base
-  belongs_to :feed_item
-  belongs_to :profile
-end
--- app/models/feed_item.rb
+++
@@ -1,8 +0,0 @@
-class FeedItem < ActiveRecord::Base  
-  belongs_to :item, :polymorphic => true
-  has_many :feeds
-  
-  def partial
-    item.class.name.underscore
-  end
-end
