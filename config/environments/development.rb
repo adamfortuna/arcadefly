998e2854cdd341fcdd57c90e164e538d254cc53e
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-08-03 01:05:27 +0000

    Updated for s3.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = true
-config.action_controller.asset_host                  = "http://localhost:3000"
+config.action_controller.asset_host                  = "http://s3.amazonaws.com/arcadefly/images"
 
 config.log_level = :debug
 
