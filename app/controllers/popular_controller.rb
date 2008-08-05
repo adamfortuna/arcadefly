2188b440ff707ef403d1524cf20daf895fcad7e3
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-08-05 04:23:37 +0000

    Some optimization changes.

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -1,7 +1,7 @@
 class PopularController < ApplicationController
 
   def index
-    @arcades = Arcade.find(:all, :limit => 10, :group => 'arcades.name', :include => :frequentships, :conditions => 'frequentships.created_at > 04-01-2008')
-    @games = Game.find(:all, :limit => 10, :group => 'games.name', :include => :favoriteships, :conditions => 'favoriteships.created_at > 04-01-2008')
+    #@arcades = Arcade.find(:all, :limit => 10, :group => 'arcades.name', :include => :frequentships, :conditions => 'frequentships.created_at > 04-01-2008')
+    #@games = Game.find(:all, :limit => 10, :group => 'games.name', :include => :favoriteships, :conditions => 'favoriteships.created_at > 04-01-2008')
   end
 end
