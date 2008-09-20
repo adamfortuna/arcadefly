88630cf40653c33f38016411f68a58688f7c8992
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-09-20 18:06:02 +0000

    Fix for dynamic region js.

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -1,8 +1,11 @@
 class JavascriptsController < ApplicationController
   caches_page :dynamic_regions
 
+  def dynamic_regions
+    @regions = Region.find(:all, :order => 'name')
+  end
+
   def dynamic_games
     @games = Game.find(:all, :order => 'name', :limit => 100)
   end
-
 end
