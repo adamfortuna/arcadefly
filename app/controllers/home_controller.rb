fe62a2a0ea722eb082a890321e384e3c6f3a5b12
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-09-21 06:16:07 +0000

    added popular stuff to home.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
   def index
+    @arcades = Arcade.find(:all, :order => 'frequentships_count desc', :limit => 10)
+    @games = Game.find(:all, :order => 'favoriteships_count desc', :limit => 10)
+    @profiles = Profile.find(:all, :order => 'created_at desc', :conditions => ['active=?', true], :limit => 10)
   end
   
   def about
