8113d5c7e473f77d0ae47b7729fe6b00f3b73e2a
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-09-22 22:43:30 +0000

    Made the lists on the home page not cached.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @arcades = Arcade.find(:all, :order => 'frequentships_count desc', :limit => 10)
-    @games = Game.find(:all, :order => 'favoriteships_count desc', :limit => 10)
+    @arcades = Arcade.find(:all, :order => 'frequentships_count desc, playables_count desc', :include => [ { :address => [:country, :region] }], :limit => 10)
+    @games = Game.find(:all, :order => 'favoriteships_count desc, playables_count desc', :limit => 10)
     @profiles = Profile.find(:all, :order => 'created_at desc', :conditions => ['active=?', true], :limit => 10)
   end
   
