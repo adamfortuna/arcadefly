74ef698cd5dfa37b4be5a425b1c6234449ad3394
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-09-25 02:39:01 +0000

    Convenience method for hours.

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -7,6 +7,10 @@ class Hour < ActiveRecord::Base
     "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM",
     "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM", "12:00 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:00 AM", "3:30 AM", "4:00 AM"]
   END_OPTIONS = START_OPTIONS
+
+  def open?
+    !closed?
+  end
   
   def self.new_week(timeable)
     [ Hour.new(:dayofweek => 'mon', :start => "10:00 AM", :end => "11:00 PM", :day => 0, :closed => false, :timeable => timeable),
