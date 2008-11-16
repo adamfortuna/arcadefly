e37ab2580aefa2c540226e222b79852bf3f30e6b
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2008-11-16 07:34:09 +0000

    Updated with new recipes for passenger.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,17 @@ depend :remote, :gem, "mislav-will_paginate", "~> 2.2"
 after "deploy:update_code", "deploy:rails_symlink"
 after "deploy:update_code", "deploy:build_assets"
 namespace :deploy do
+
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+
   task :rails_symlink do
     run "ln -nfs #{deploy_to}/#{shared_dir}/rails #{release_path}/vendor/rails"
   end
@@ -31,20 +42,10 @@ namespace :deploy do
     run <<-EOF
       cd #{release_path} && rake RAILS_ENV=production asset:packager:build_all
     EOF
-  end
-  
+  end  
 end
-
 
 before "deploy:symlink", "s3_asset_host:synch_public"
-
-namespace :passenger do
-  desc "Restart Application"
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-end
-after :deploy, "passenger:restart"
 
 task :after_update_code, :roles => :app do
   run "rm -rf #{release_path}/public/.htaccess"
