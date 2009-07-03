298700a14345070f120532fb77f4987409a71615
Adam Fortuna <adam@fortuna.name = Adam Fortuna adam@fortuna.name>
2009-07-03 01:55:40 +0000

    Disabling caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,15 +3,15 @@
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
-config.cache_store = :mem_cache_store, 'localhost:11211'
+#config.cache_store = :mem_cache_store, 'localhost:11211'
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-ActionController::Base.perform_caching               = true
+config.action_controller.perform_caching             = false
+ActionController::Base.perform_caching               = false
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # Disable delivery errors, bad email addresses will be ignored
