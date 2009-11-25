1da9a1bdcc3e7c39a3ff0a52bff362be38922a35
Adam Fortuna <adam@fortuna.name>
2009-11-24 20:06:43 -0500

    Updated for removed plugins and various other changes

--- config/environment.rb
+++ config/environment.rb
@@ -61,7 +61,7 @@ Rails::Initializer.run do |config|
   # config.gem 'rspec-rails',             :lib => false,            :source => 'http://gems.rubyforge.org/', :version => '~>1.2.2'
   # config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl',   :source => 'http://gems.github.com'
   # config.gem 'thoughtbot-shoulda',      :lib => false,            :source => 'http://gems.github.com'
-  config.gem 'mislav-will_paginate',    :lib => 'will_paginate',  :source => 'http://gems.github.com',     :version => '~> 2.3.7'
-  config.gem 'andre-geokit',            :lib => 'geokit',         :source => 'http://gems.github.com',     :version => '~> 1.2.2'
-  config.gem 'avatar',                  :lib => 'avatar',         :source => 'http://gems.github.com',     :version => '~> 0.2.0'
+  # config.gem 'mislav-will_paginate',    :lib => 'will_paginate',  :source => 'http://gems.github.com',     :version => '~> 2.3.7'
+  # config.gem 'andre-geokit',            :lib => 'geokit',         :source => 'http://gems.github.com',     :version => '~> 1.2.2'
+  # config.gem 'avatar',                  :lib => 'avatar',         :source => 'http://gems.github.com',     :version => '~> 0.2.0'
 end
---
+++ public/javascripts/arcadefly_packaged.js
@@ -0,0 +1,1 @@
+function toggleLabel(element,show){show&&$("#"+element).val()==''?$("#"+element+'_label').show():$("#"+element+'_label').hide();}
